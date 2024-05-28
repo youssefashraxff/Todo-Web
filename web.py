07eeb6c5f85c83b88176c05727279f351c3eeda9
youssef <y.ashraf.ibrahim@gmail.com>
2024-05-28 22:47:57 +0300

    initial commit for web tood

---
+++ web.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from todo_functions import getTodos,setTodos
+
+todos = getTodos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    setTodos(todos)
+   
+st.title("Todoie")
+
+for i,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(i)
+        setTodos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label='',placeholder="Add a new task.",
+              on_change=add_todo,key="new_todo")    
+
+#st.session_state
